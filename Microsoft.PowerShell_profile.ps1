d72ef95b96ae6f4cfe38fb36e9e60f7b5f7b3229
Sef <severin.fitriyadi@gmail.com>
2022-11-29 13:57:36 +0100

    Autoinstall Terminal-Icons

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,9 @@
 oh-my-posh init pwsh --config $env:POSH_THEMES_PATH\sef.omp.json | Invoke-Expression
 
+if (!(Get-Module -ListAvailable -Name Terminal-Icons)) {
+    Install-Module -Name Terminal-Icons -Repository PSGallery
+}
+
 Import-Module -Name Terminal-Icons
 
 $PSSTYLE.FileInfo.Directory = "`e[38;2;67;174;209m"
